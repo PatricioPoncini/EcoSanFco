b2de2e97e17db16e463dff999b0386795d09d63f
PatricioPoncini <patricioponcini10@gmail.com>
2023-10-24 20:30:33 -0300

    add express server config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # EcoSanFco
-Proyecto para que los usuarios puedan subir reclamos referidos a cuidado del medio ambiente, como reclamo por basural al cieo abierto, basura tirada, incendios, etc
+Proyecto para que los vecinos de San Francisco puedan subir reclamos referidos a cuidado del medio ambiente, como reclamo por basural a cielo abierto, basura tirada en calles, incendios, etc
 
 ![EcoSanFco logo](./ecosanfcologo.png)
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+async function main() {
+    try {
+        app.listen(4040);
+        console.log("Server on port", 1110);
+    } catch (error) {
+        console.log("Database connection error");
+        console.log(error);
+    }
+}
+
+main();
